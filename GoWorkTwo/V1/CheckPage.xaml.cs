03c10270a2fb4ed67d0fd447bca9c33a02039ad9
Mohd Zulhilmi Mohd Zain <m.zulhilmi@msd.net.my>
2018-10-23 11:29:36 +0800

    interface

---
+++ GoWorkTwo/V1/CheckPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace GoWorkTwo.V1
+{
+    public partial class CheckPage : ContentPage
+    {
+        public CheckPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
